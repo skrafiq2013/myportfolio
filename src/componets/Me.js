a3b476fc46e39ba6961153a5173d687ac891c4ab
Rafiq <sk.rafiq2013@gmail.com>
2022-10-03 11:51:08 +0530

    margin removed

--- src/componets/Me.js
+++ src/componets/Me.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Me() {
   return (
     <div>
-      <div className=' container mt-3 container-1'>
+      <div className=' container  container-1'>
         <div className='pt-4'>
         <h1 >Frontend Developer</h1>
         </div>
